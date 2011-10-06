3eda6c9ef0d7d1eac49e1c5ca7c815846c8483d8
marcovena <marcovena@libero.it>
2011-10-06 18:32:38 +0200

    script nodes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 (function(){
-alert('Message');
+
+var c = document.getElementById("content");
+c.style.border = "1px solid red";
+
+var items = new Array('Zoff','Burnich','Facch\u8888xxxetti');
+var list = document.createElement("ul");
+	
+for (var i=0;i<items.lenght;i++)
+{
+	var li = document.createElement("li");
+	li.innerText = items[i];
+	list.appendChild("li");
+	console.log(items[i]);
+}
+
 
 }
 )()
